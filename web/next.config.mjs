d370951e05cbf2faf47823b4f65ee6c1db44df17
Abdelrahman Elasrag <realabdelrahmanelasrag@gmail.com>
2026-02-20 13:24:50 +0200

    hotfix: image upload use blob in vercel

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["firebase-admin"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
